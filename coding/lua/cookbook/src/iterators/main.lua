a85b34bd356460debf8bea38dd6cf6eeb5514289
projetmbc <projetmbc@gmail.com>
2025-09-14 12:06:14 +0200

    [lua] Starting Lua tutos

---
+++ coding/lua/cookbook/src/iterators/main.lua
@@ -0,0 +1,26 @@
+------
+-- prototype::
+--     imax :
+--     jmax :
+--     kmax :
+------
+function iter3D(imax, jmax, kmax)
+    return coroutine.wrap(
+        function()
+            for k = 1, kmax do
+                for j = 1, jmax do
+                    for i = 1, imax do
+                        coroutine.yield(i, j, k)
+                    end
+                end
+            end
+        end
+    )
+end
+
+-- DEMO --
+
+for x, y, z in iter3D(2, 3, 4) do
+    print("-----")
+  print(x .. ";" .. y .. ";" .. z)
+end
