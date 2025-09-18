ee5363c8245bbc725ff67d9d81820956ef5c99d3
projetmbc <projetmbc@gmail.com>
2025-09-18 18:13:46 +0200

    [science-tools] luadraw - Diagrams with sets #8

---
+++ science-tools/latex-and-co/cookbook/src/graphic/set-theory-n-co/venn-diagram/2-sets/no-universe/main.lua
@@ -0,0 +1,14 @@
+---------------------------
+-- Factorisation du code --
+---------------------------
+i = cpx.I
+
+------
+-- prototype::
+--     c : JJJJ
+--
+--     :return: XXXX
+------
+function SET_COL(c)
+  return "color = " .. c .. ", pattern color = " .. c
+end
