a027237a0c2be5861a9c402b367443d9136bbcdd
projetmbc <projetmbc@gmail.com>
2025-09-19 11:05:17 +0200

    [science-tools] luadraw - 3D repr. of a projective plane #1

---
+++ science-tools/latex-and-co/cookbook/src/graphic/3D/surface/projective-plane/common-lua/spectrum.lua
@@ -0,0 +1,88 @@
+------
+-- protottype::
+--     facets :
+--
+--     :return:
+------
+function get_bary_bbox(facets)
+  local update_extreme_vals = function (a, a_min, a_max)
+    return math.min(a, a_min), math.max(a, a_max)
+  end
+
+  local G, level
+
+  local xmin, xmax = math.huge, -math.huge
+  local ymin, ymax = math.huge, -math.huge
+  local zmin, zmax = math.huge, -math.huge
+
+  for _ , f in ipairs(facets) do
+    G = isobar3d(f)
+
+    xmin, xmax = update_extreme_vals(G.x, xmin, xmax)
+    ymin, ymax = update_extreme_vals(G.y, ymin, ymax)
+    zmin, zmax = update_extreme_vals(G.z, zmin, zmax)
+  end
+
+  return {xmin, xmax, ymin, ymax, zmin, zmax}
+end
+
+------
+-- protottype::
+--     surface  :
+--     spectrum :
+--     dir      :
+--
+--     :action:
+------
+function graph:draw_spectrum(surface, spectrum, dir)
+  local bary_bbox = get_bary_bbox(surface)
+
+  local sorted_facets = self:Sortfacet(surface)
+
+  local imin, imax
+
+  if dir == "x" then
+    imin, imax = 1, 2
+
+  elseif dir == "y" then
+    imin, imax = 3, 4
+
+  elseif dir == "z" then
+    imin, imax = 5, 6
+  end
+
+  local val_min   = bary_bbox[imin]
+  local delta_val = bary_bbox[imax] - val_min
+
+  local G, val, level
+
+  for _ , f in ipairs(sorted_facets) do
+    G = isobar3d(f)
+
+    if dir == "x" then
+      val = G.x
+
+    elseif dir == "y" then
+      val = G.y
+
+    else
+      val = G.z
+    end
+
+    level = math.abs((val - val_min) / delta_val)
+
+    self:Dpolyline3d(f, true, "fill=" .. palette(spectrum, level))
+  end
+end
+
+function graph:draw_Xspectrum(surface, spectrum)
+  self:draw_spectrum(surface, spectrum, "x")
+end
+
+function graph:draw_Yspectrum(surface, spectrum)
+  self:draw_spectrum(surface, spectrum, "y")
+end
+
+function graph:draw_Zspectrum(surface, spectrum)
+  self:draw_spectrum(surface, spectrum, "z")
+end
