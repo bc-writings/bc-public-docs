c9cc9afe0961e5e68d8730173723adb065623580
projetmbc <projetmbc@gmail.com>
2025-09-20 19:37:33 +0200

    [science-tools] luadraw - Data OK

--- science-tools/latex-and-co/cookbook/src/graphic/data/csv/common/csv.lua
+++ science-tools/latex-and-co/cookbook/src/graphic/data/csv/common/csv.lua
@@ -1,20 +1,35 @@
-function read_CSV(file, ignore_header)
-  local data = {}
+------
+-- prototype::
+--     file        : le chemin du fichier à analyser.
+--     have_header : réglé à ''true'', ce ¨param demande d'ignorer
+--                   la ¨1ere ligne, sinon ¨ttes les lignes sont
+--                   lues.
+--
+--     :return: un tableau de sous-tableaux représentant les
+--              données extraites ligne par ligne.
+------
+function read_CSV(file, have_header)
+    local data = {}
 
-  for line in io.lines(file) do
-    if ignore_header then
-      ignore_header = false
+    for line in io.lines(file) do
+      if have_header then
+        have_header = false
 
-    else
-      local result = {}
+      else
+        local result = {}
 
-      for part in string.gmatch(line, "([^,]+)") do
-        table.insert(result, tonumber(part))
+  ------
+  -- Usage d'expressions rationnelles pour autoriser de futures
+  -- améliorations techniques. Pour le moment les groupes capturés
+  -- sont formés de caractères autres que la virgule.
+  ------
+        for part in string.gmatch(line, "([^,]+)") do
+          table.insert(result, tonumber(part))
+        end
+
+        table.insert(data, result)
       end
-
-      table.insert(data, result)
     end
-  end
 
-  return data
-end
+    return data
+  end
