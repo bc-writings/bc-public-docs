7625a40745dea224c419ef9763472ea1765f162a
projetmbc <projetmbc@gmail.com>
2025-11-23 17:00:22 +0100

    [science-tools] latex-and-co - Shadow of cube animated - New OK

--- science-tools/tools/02-crop-forced-pdfs.sh
+++ science-tools/tools/02-crop-forced-pdfs.sh
@@ -32,7 +32,7 @@ fi
 
 cd "$TARGET"
 
-for f in $(find cookbook/src -name 'main*.tex' | sort -u)
+for f in $(find cookbook/src -name 'main*.tex' -o -name 'xtra*.tex' | sort -u)
 do
   if grep -q '^[[:space:]]*%[[:space:]]*FORCE-CROP[[:space:]]*$' "$TARGET/$f"
   then
