8af5d3b1f85be718f5420b634bdcfd4ad9cbfbc8
projetmbc <projetmbc@gmail.com>
2026-06-14 18:25:58 +0200

    [science-tools] latex-and-co - Preparing online version #8 - Septies

--- science-tools/latex-and-co/cookbook/src/_dev/data/csv/plot/common/csv.lua
+++ science-tools/latex-and-co/cookbook/src/_dev/data/csv/plot/common/csv.lua
@@ -1,34 +1,41 @@
 ------
 -- prototype::
 --     file        : le chemin du fichier à analyser.
---     have_header : réglé à ''true'', ce ¨param demande d'ignorer
---                   la ¨1ere ligne, sinon ¨ttes les lignes sont
---                   lues.
+--                 @ type(have_header) = path
+--     have_header : ''true'' demande d'ignorer la \1ere ligne,
+--                   sinon \ttes les lignes sont lues.
+--                 @ type(have_header) = bool
 --
---     :return: un tableau de sous-tableaux représentant les
---              données extraites ligne par ligne.
+--     :return: un \tab de sous-\tabs représentant les données
+--              extraites ligne par ligne.
 ------
 function read_CSV(file, has_header)
--- Laissons une trace du fichier analysé dans le fichier \log. Ceci
--- permet, par exemple, à \latexmk de savoir qu'il doit compiler de
--- nouveau un document en cas de changements dans les fichiers de
--- données utilisés.
+------
+-- note::
+--     Nous laissons une trace du fichier analysé dans le fichier
+--     \log géré par \latex.
+--     Ceci permet, par exemple, à \latexmk de savoir qu'il doit
+--     compiler de nouveau un \docu en cas de \chgts dans les
+--     fichiers de données.
+------
     texio.write_nl("(" .. file .. ")")
 
     local data = {}
 
     for line in io.lines(file) do
+-- Ligne ignorée (uniquement la \1ere)
       if has_header then
         has_header = false
 
+-- Ligne analysée.
       else
         local result = {}
 
-  ------
-  -- Usage d'expressions rationnelles pour autoriser de futures
-  -- améliorations techniques. Pour le moment les groupes capturés
-  -- sont formés de caractères autres que la virgule.
-  ------
+------
+-- Usage d'tegex_frs pour autoriser de futures améliorations
+-- \techs. Pour le moment les \gpes capturés sont formés de
+-- \caracs autres que la virgule.
+------
         for part in string.gmatch(line, "([^,]+)") do
           table.insert(result, tonumber(part))
         end
